917f782241bf95eb1e3aae32ed91a529e8fb0065
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-05-28 14:12:09 +0400

    Added University model class

---
+++ src/main/java/com/papook/portfolio3/models/University.java
@@ -0,0 +1,30 @@
+package com.papook.portfolio3.models;
+
+import java.net.URL;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class University {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String country;
+    String department;
+    URL link;
+    String contactPersonName;
+    Integer outgoingStudentNumber;
+    Integer incomingStudentNumber;
+    LocalDate nextSpringSemesterStart;
+    LocalDate nextFallSemesterStart;
+}
