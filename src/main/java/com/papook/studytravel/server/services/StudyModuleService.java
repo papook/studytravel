4c0f489d055561c906e1e93e4ced241f1ae9f6b1
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-05-28 20:32:13 +0400

    Added functions to the StudyModuleService interface for creating an updating a module and assigning it directly to a university

--- src/main/java/com/papook/studytravel/server/services/StudyModuleService.java
+++ src/main/java/com/papook/studytravel/server/services/StudyModuleService.java
@@ -6,13 +6,19 @@ import java.util.Optional;
 import com.papook.studytravel.server.models.StudyModule;
 
 public interface StudyModuleService {
+
     public Iterable<StudyModule> getAllModules();
 
     public Optional<StudyModule> getModuleById(Long id);
 
     public URI createModule(StudyModule module);
 
+    public URI createModuleForUniversity(Long universityId, StudyModule module);
+
     public Optional<URI> updateModule(Long id, StudyModule module);
 
+    public Optional<URI> updateModuleForUniversity(Long universityId, Long moduleId, StudyModule module);
+
     public void deleteModule(Long id);
+
 }
