cb6dfdf6d7c14e4691e54b11827d1a67f6df338c
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-05-28 20:00:00 +0400

    Created ModuleController

---
+++ src/main/java/com/papook/studytravel/server/controllers/ModuleController.java
@@ -0,0 +1,12 @@
+package com.papook.studytravel.server.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.papook.studytravel.server.ServerConfiguration;
+
+@RestController
+@RequestMapping(ServerConfiguration.API_BASE)
+public class ModuleController{
+
+}
