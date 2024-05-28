e1103d5bf91854584e75ae54f63b4d8a5e13cfa1
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-05-28 19:51:02 +0400

    added universityId field

--- src/main/java/com/papook/studytravel/server/models/Module.java
+++ src/main/java/com/papook/studytravel/server/models/Module.java
@@ -1,14 +1,18 @@
 package com.papook.studytravel.server.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * Represents a module in a study program.
+ * Represents a module that can be taken by students
+ * at a partner university.
  */
 @Data
 @NoArgsConstructor
@@ -21,6 +25,9 @@ public class Module {
     String name;
     Semester semester;
     Integer creditPoints;
+    @JsonIgnore
+    @ManyToOne
+    Long universityId;
 
     /**
      * Represents the semester in which the module is offered.
