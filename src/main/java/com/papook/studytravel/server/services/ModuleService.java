54733f6cfac8a513a1aeac33077e07a2b4236b77
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-05-28 20:03:20 +0400

    Created a basic ModuleService interface for CRUD operations on modules

---
+++ src/main/java/com/papook/studytravel/server/services/ModuleService.java
@@ -0,0 +1,16 @@
+package com.papook.studytravel.server.services;
+
+import java.net.URI;
+import java.util.Optional;
+
+public interface ModuleService {
+    public Iterable<Module> getAllModules();
+
+    public Optional<Module> getModuleById(Long id);
+
+    public URI createModule(Module module);
+
+    public Optional<URI> updateModule(Long id, Module module);
+
+    public void deleteModule(Long id);
+}
