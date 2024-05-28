de7633e954502b6416d501ae155923d84335128c
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-05-28 20:32:45 +0400

    removed unnecessary import statement

--- src/main/java/com/papook/studytravel/server/models/StudyModule.java
+++ src/main/java/com/papook/studytravel/server/models/StudyModule.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
