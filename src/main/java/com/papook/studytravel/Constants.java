d532225ad7b04d4908c9a3644457904f58fbd335
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-05-28 18:15:35 +0400

    hardcoded port number in the constants

--- src/main/java/com/papook/studytravel/Constants.java
+++ src/main/java/com/papook/studytravel/Constants.java
@@ -1,13 +1,8 @@
 package com.papook.studytravel;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-
 public class Constants {
-    @Autowired
-    public static Environment env;
 
-    public static final String PORT = env.getProperty("server.port");
+    public static Integer PORT = 8080;
     public static final String API_BASE = "/api";
     public static final String BASE_URI = "localhost:" + PORT + API_BASE;
 
