2235c53f2683b24c90fd06a23108c77fd40968f4
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-05-28 19:27:15 +0400

    AddedMODULE_BASE constant for modules endpoint

--- src/main/java/com/papook/studytravel/Constants.java
+++ src/main/java/com/papook/studytravel/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final String BASE_URI = "localhost:" + PORT + API_BASE;
 
     public static final String UNIVERSITY_BASE = "/universities";
+    public static final String MODULE_BASE = "/modules";
 }
