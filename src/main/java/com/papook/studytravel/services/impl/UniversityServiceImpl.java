cbc70d821de3ad42738c8f47093dbe93783ec1d6
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-05-28 16:34:37 +0400

    Implemented UniversityService base functionality

---
+++ src/main/java/com/papook/studytravel/services/impl/UniversityServiceImpl.java
@@ -0,0 +1,94 @@
+package com.papook.studytravel.services.impl;
+
+import java.net.URI;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.papook.studytravel.Constants;
+import com.papook.studytravel.models.University;
+import com.papook.studytravel.repositories.UniversityRepository;
+import com.papook.studytravel.services.UniversityService;
+
+@Service
+public class UniversityServiceImpl implements UniversityService {
+
+    @Autowired
+    UniversityRepository repository;
+
+    /**
+     * Retrieves all universities from the database.
+     *
+     * @return An iterable collection of universities.
+     */
+    @Override
+    public Iterable<University> getAllUniversities() {
+        Iterable<University> result = repository.findAll();
+        return result;
+    }
+
+    /**
+     * Retrieves a university by its ID from the database.
+     *
+     * @param id The ID of the university to retrieve.
+     * @return An optional containing the university, or an empty optional if the
+     *         university does not exist.
+     */
+    @Override
+    public Optional<University> getUniversityById(Long id) {
+        Optional<University> result = repository.findById(id);
+        return result;
+    }
+
+    /**
+     * Creates a new university in the database.
+     *
+     * @param university The university object to create.
+     * @return The URI representing the location of the newly created university.
+     */
+    @Override
+    public URI createUniversity(University university) {
+        University result = repository.save(university);
+        URI location = URI.create(Constants.BASE_URI + Constants.UNIVERSITY_BASE + "/" + result.getId());
+        return location;
+    }
+
+    /**
+     * If the university with the given id exists, update it with the given
+     * university and return an empty optional.
+     * Otherwise, create a new university and return an optional containing the URI
+     * of the new university.
+     *
+     * @param id         The ID of the university to update.
+     * @param university The university object.
+     * @return An optional URI representing the location of the newly created
+     *         university, or an empty optional if the university already exists and
+     *         was updated.
+     */
+    @Override
+    public Optional<URI> updateUniversity(Long id, University university) {
+        Optional<University> existing = repository.findById(id);
+        if (existing.isPresent()) {
+            university.setId(id);
+            repository.save(university);
+            return Optional.empty();
+        } else {
+            University result = repository.save(university);
+            URI location = URI.create(Constants.BASE_URI + Constants.UNIVERSITY_BASE + "/" + result.getId());
+            return Optional.of(location);
+        }
+    }
+
+    /**
+     * Deletes a university from the database.
+     *
+     * @param id The ID of the university to delete.
+     * @throws IllegalArgumentException if the university does not exist.
+     */
+    @Override
+    public void deleteUniversity(Long id) {
+        repository.deleteById(id);
+    }
+
+}
