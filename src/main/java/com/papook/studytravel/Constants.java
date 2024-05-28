6e96cd970ccdec69f12fcff09859aece2596b261
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-05-28 16:01:02 +0400

    created a constants file containig url information for more modularity

---
+++ src/main/java/com/papook/studytravel/Constants.java
@@ -0,0 +1,13 @@
+package com.papook.studytravel;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
+public class Constants {
+    @Autowired
+    public static Environment env;
+    
+    public static final String PORT = env.getProperty("server.port");
+    public static final String API_BASE = "/api";
+    public static final String BASE_URI = "localhost:" + PORT + API_BASE;
+}
--- src/main/java/com/papook/studytravel/controllers/ApplicationController.java
+++ src/main/java/com/papook/studytravel/controllers/ApplicationController.java
@@ -2,9 +2,10 @@ package com.papook.studytravel.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.papook.studytravel.Constants;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(Constants.API_BASE)
 public class ApplicationController {
     @RequestMapping("/hello")
     public String hello() {
