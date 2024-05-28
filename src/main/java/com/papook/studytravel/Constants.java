05c7602d91026ca3e5fae90aac3bc65e73e408e5
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-05-28 16:10:31 +0400

    Added UNIVERSITY_BASE constant for universities endpoint

--- src/main/java/com/papook/studytravel/Constants.java
+++ src/main/java/com/papook/studytravel/Constants.java
@@ -6,8 +6,10 @@ import org.springframework.core.env.Environment;
 public class Constants {
     @Autowired
     public static Environment env;
-    
+
     public static final String PORT = env.getProperty("server.port");
     public static final String API_BASE = "/api";
     public static final String BASE_URI = "localhost:" + PORT + API_BASE;
+
+    public static final String UNIVERSITY_BASE = "/universities";
 }
