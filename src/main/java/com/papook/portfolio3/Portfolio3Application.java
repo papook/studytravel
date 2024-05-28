aa2b64e55d4119bef4549b74a369e6d6dadb3a51
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-05-28 13:57:25 +0400

    Spring boot project created

---
+++ src/main/java/com/papook/portfolio3/Portfolio3Application.java
@@ -0,0 +1,13 @@
+package com.papook.portfolio3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Portfolio3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Portfolio3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/papook/portfolio3/Portfolio3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papook.portfolio3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Portfolio3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
