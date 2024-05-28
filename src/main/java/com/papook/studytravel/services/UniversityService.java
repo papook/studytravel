096ff3bf9e8b52fb911df5bfca3eac4d6e6b4fc7
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-05-28 16:06:25 +0400

    changed UniversityService createUniversity method's return type to URI

--- src/main/java/com/papook/studytravel/services/UniversityService.java
+++ src/main/java/com/papook/studytravel/services/UniversityService.java
@@ -1,5 +1,6 @@
 package com.papook.studytravel.services;
 
+import java.net.URI;
 import java.util.Optional;
 
 import com.papook.studytravel.models.University;
@@ -9,7 +10,7 @@ public interface UniversityService {
 
     public Optional<University> getUniversityById(Long id);
 
-    public University createUniversity(University university);
+    public URI createUniversity(University university);
 
     public University updateUniversity(Long id, University university);
 
