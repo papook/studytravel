6b2e94a0ff783701ff443e567f98dc369bf146e3
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-05-29 19:19:41 +0400

    documented the ServerConfiguration class

--- src/main/java/com/papook/studytravel/server/ServerConfiguration.java
+++ src/main/java/com/papook/studytravel/server/ServerConfiguration.java
@@ -1,12 +1,35 @@
 package com.papook.studytravel.server;
 
+/**
+ * The ServerConfiguration class represents
+ * the configuration settings for the server.
+ */
 public class ServerConfiguration {
 
-    public static Integer PORT = 8080;
-    public static String HOST = "localhost";
-    public static String BASE_URI = "http://" + HOST + ":" + PORT;
+    /**
+     * The port number on which the server is running.
+     */
+    public static final Integer PORT = 8080;
 
+    /**
+     * The host name of the server.
+     */
+    public static final String HOST = "localhost";
+
+    /**
+     * The base URI of the server
+     * constructed from the host and port.
+     */
+    public static final String BASE_URI = "http://" + HOST + ":" + PORT;
+
+    /**
+     * The base path for university-related endpoints.
+     */
     public static final String UNIVERSITY_BASE = "/universities";
+
+    /**
+     * The base path for module-related endpoints.
+     */
     public static final String MODULE_BASE = "/modules";
 
 }
