175aba97cecdc50c0b9cf353a199346f1d19fae6
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-05-29 16:21:46 +0400

    Refactored ServerConfiguration for better modularity

--- src/main/java/com/papook/studytravel/server/ServerConfiguration.java
+++ src/main/java/com/papook/studytravel/server/ServerConfiguration.java
@@ -3,9 +3,10 @@ package com.papook.studytravel.server;
 public class ServerConfiguration {
 
     public static Integer PORT = 8080;
-    public static final String API_BASE = "/api";
-    public static final String BASE_URI = "localhost:" + PORT + API_BASE;
+    public static String HOST = "localhost";
+    public static String BASE_URI = "http://" + HOST + ":" + PORT;
 
     public static final String UNIVERSITY_BASE = "/universities";
     public static final String MODULE_BASE = "/modules";
+
 }
