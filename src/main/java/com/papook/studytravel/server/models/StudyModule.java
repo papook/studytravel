3348e648d247595fc1d2a30483ac78df83166f5b
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-09 21:51:13 +0400

    added custom Json setter for semester variable of StudyModule

--- src/main/java/com/papook/studytravel/server/models/StudyModule.java
+++ src/main/java/com/papook/studytravel/server/models/StudyModule.java
@@ -1,11 +1,15 @@
 package com.papook.studytravel.server.models;
 
+import com.fasterxml.jackson.annotation.JsonSetter;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Represents a module that can be taken by students
@@ -20,8 +24,14 @@ public class StudyModule {
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     Long id;
     String name;
+    @Setter(AccessLevel.NONE)
     Semester semester;
     Integer creditPoints;
+
+    @JsonSetter
+    public void setSemester(String semester) {
+        this.semester = Semester.valueOf(semester.toUpperCase());
+    }
 
     /**
      * Represents the semester in which the module is offered.
