63fb2a666f3f2a5065f388b0d667f9e09039f608
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-09 23:39:18 +0400

    exception handler for UniversityNotFoundException

--- src/main/java/com/papook/studytravel/server/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/papook/studytravel/server/controllers/GlobalExceptionHandler.java
@@ -8,31 +8,45 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.papook.studytravel.server.errors.ErrorMessage;
+import com.papook.studytravel.server.errors.UniversityNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ErrorMessage> handleException(Exception ex) {
-        HttpStatus status = HttpStatus.BAD_REQUEST;
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<ErrorMessage> handleException(Exception ex) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 
-        ErrorMessage errorMessage = new ErrorMessage(
-                status.value(),
-                "Could not read the request body. Please provide a valid JSON object.");
+		ErrorMessage errorMessage = new ErrorMessage(
+				status.value(),
+				"Could not read the request body. Please provide a valid JSON object.");
 
-        return ResponseEntity.status(status)
-                .body(errorMessage);
-    }
+		return ResponseEntity.status(status)
+				.body(errorMessage);
+	}
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorMessage> handleException(MethodArgumentNotValidException ex) {
-        HttpStatus status = HttpStatus.BAD_REQUEST;
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorMessage> handleException(MethodArgumentNotValidException ex) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 
-        ErrorMessage errorMessage = new ErrorMessage(
-                status.value(),
-                "The request body contains invalid data. Make sure all fields are provided and have the correct format.");
+		ErrorMessage errorMessage = new ErrorMessage(
+				status.value(),
+				"The request body contains invalid data. Make sure all fields are provided and have the correct format.");
 
-        return ResponseEntity.status(status)
-                .body(errorMessage);
-    }
+		return ResponseEntity.status(status)
+				.body(errorMessage);
+	}
+
+	@ExceptionHandler(UniversityNotFoundException.class)
+	public ResponseEntity<ErrorMessage> handleException(UniversityNotFoundException ex) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+
+		ErrorMessage errorMessage = new ErrorMessage(
+				status.value(),
+				"The requested university was not found. Please provide a valid university ID.");
+
+		return ResponseEntity.status(status)
+				.body(errorMessage);
+	}
+
 }
