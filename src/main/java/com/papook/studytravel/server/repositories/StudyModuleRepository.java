8789a046dea49efda8f60f440b8541d0f1242967
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-09 16:37:31 +0400

    removed unnecessary custom function form StudyModuleRepository

--- src/main/java/com/papook/studytravel/server/repositories/StudyModuleRepository.java
+++ src/main/java/com/papook/studytravel/server/repositories/StudyModuleRepository.java
@@ -1,12 +1,10 @@
 package com.papook.studytravel.server.repositories;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
 import com.papook.studytravel.server.models.StudyModule;
 
 @Repository
 public interface StudyModuleRepository extends CrudRepository<StudyModule, Long> {
-    List<StudyModule> findByUniversityId(Long universityId);
 }
