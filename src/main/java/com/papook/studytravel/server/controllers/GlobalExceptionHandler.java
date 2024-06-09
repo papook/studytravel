02b699275882dfb03b823fc493ab5f9541ad7f34
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-09 21:49:17 +0400

    added an exception handler for HttpMessageNotReadableException

--- src/main/java/com/papook/studytravel/server/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/papook/studytravel/server/controllers/GlobalExceptionHandler.java
@@ -1,8 +1,26 @@
 package com.papook.studytravel.server.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.papook.studytravel.server.errors.ErrorMessage;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorMessage> handleException(Exception ex) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        ErrorMessage errorMessage = new ErrorMessage(
+                status.value(),
+                "Could not read the request body. Please provide a valid JSON object.");
+
+        return ResponseEntity.status(status)
+                .body(errorMessage);
+    }
 
 }
