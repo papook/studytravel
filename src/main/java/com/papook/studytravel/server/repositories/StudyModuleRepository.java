0136dee2caf5502fa94fdc0b6e3022a79760cf29
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-11 17:01:37 +0400

    method added to filter and page study modules based on name and semester

--- src/main/java/com/papook/studytravel/server/repositories/StudyModuleRepository.java
+++ src/main/java/com/papook/studytravel/server/repositories/StudyModuleRepository.java
@@ -1,5 +1,7 @@
 package com.papook.studytravel.server.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,8 @@ import com.papook.studytravel.server.models.StudyModule;
 
 @Repository
 public interface StudyModuleRepository extends CrudRepository<StudyModule, Long> {
+    public Page<StudyModule> findByNameContainingAndSemesterIgnoreCase(
+            String name,
+            String semester,
+            Pageable pageable);
 }
