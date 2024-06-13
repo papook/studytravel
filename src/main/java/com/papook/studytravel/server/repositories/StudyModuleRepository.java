45e02a3cd8f10da4b11c90b95468cf3ad00bb586
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-13 12:56:16 +0400

    javadoc added for custom methods of repositories

--- src/main/java/com/papook/studytravel/server/repositories/StudyModuleRepository.java
+++ src/main/java/com/papook/studytravel/server/repositories/StudyModuleRepository.java
@@ -9,11 +9,41 @@ import com.papook.studytravel.server.models.StudyModule;
 
 @Repository
 public interface StudyModuleRepository extends CrudRepository<StudyModule, Long> {
+    /**
+     * Find study modules by name and semester containing the given strings.
+     * This method is case-insensitive.
+     * 
+     * @param name     The study module name to search for.
+     * @param semester The semester name to search for.
+     * @param pageable The pageable object to use for pagination.
+     * 
+     * @return A page of study modules that match the search criteria.
+     * 
+     * @see org.springframework.data.domain.Page
+     * @see org.springframework.data.domain.Pageable
+     * 
+     * @author papook
+     */
     public Page<StudyModule> findByNameContainingAndSemesterContainingIgnoreCase(
             String name,
             String semester,
             Pageable pageable);
 
+    /**
+     * Find study modules by university ID, name and semester containing the given
+     * strings. This method is case-insensitive.
+     * 
+     * @param universityId The university ID to search for.
+     * @param name         The study module name to search for.
+     * @param semester     The semester name to search for.
+     * @param pageable     The pageable object to use for pagination.
+     * @return A page of study modules that match the search criteria.
+     * 
+     * @see org.springframework.data.domain.Page
+     * @see org.springframework.data.domain.Pageable
+     * 
+     * @author papook
+     */
     public Page<StudyModule> findAllByUniversityIdAndNameContainingAndSemesterContainingIgnoreCase(
             Long universityId,
             String name,
--- src/main/java/com/papook/studytravel/server/repositories/UniversityRepository.java
+++ src/main/java/com/papook/studytravel/server/repositories/UniversityRepository.java
@@ -9,6 +9,21 @@ import com.papook.studytravel.server.models.University;
 
 @Repository
 public interface UniversityRepository extends CrudRepository<University, Long> {
+    /**
+     * Find universities by name and country containing the given strings.
+     * This method is case-insensitive.
+     * 
+     * @param name     The university name to search for.
+     * @param country  The country name to search for.
+     * @param pageable The pageable object to use for pagination.
+     * 
+     * @return A page of universities that match the search criteria.
+     * 
+     * @see org.springframework.data.domain.Page
+     * @see org.springframework.data.domain.Pageable
+     * 
+     * @author papook
+     */
     public Page<University> findByNameContainingAndCountryContainingIgnoreCase(
             String name,
             String country,
