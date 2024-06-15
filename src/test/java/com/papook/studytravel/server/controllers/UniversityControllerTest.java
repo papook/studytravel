4a83f6b166420e2106722dc90b37c05337cac050
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-15 19:36:56 +0400

    refactored test methods to improve code readability

--- src/test/java/com/papook/studytravel/server/controllers/UniversityControllerTest.java
+++ src/test/java/com/papook/studytravel/server/controllers/UniversityControllerTest.java
@@ -3,13 +3,16 @@ package com.papook.studytravel.server.controllers;
 import static com.papook.studytravel.server.ServerConfiguration.BASE_URI;
 import static com.papook.studytravel.server.ServerConfiguration.MODULE_ENDPOINT;
 import static com.papook.studytravel.server.ServerConfiguration.UNIVERSITY_ENDPOINT;
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.papook.studytravel.server.utils.HypermediaGenerator.formatLinkHeader;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.net.URI;
 import java.time.LocalDate;
@@ -49,7 +52,7 @@ public class UniversityControllerTest {
 	private ObjectMapper objectMapper;
 
 	@Test
-	public void testgetCollection() throws Exception {
+	public void testGetCollection() throws Exception {
 		int universityCount = 5;
 		List<University> universityList = generateUniversityList(universityCount);
 		Page<University> universitiesPage = new PageImpl<>(universityList);
@@ -58,20 +61,15 @@ public class UniversityControllerTest {
 		when(universityService.getUniversities("", "", 0)).thenReturn(universitiesPage);
 		when(hypermediaGenerator.buildPagingLinksHeaders(universitiesPage)).thenReturn(new HttpHeaders());
 
-		// Get the response from the controller
-		var response = mockMvc.perform(get(UNIVERSITY_ENDPOINT))
-				.andExpect( // Test the content length of the response
-						jsonPath("$.length()").value(universityCount))
+		mockMvc.perform(get(UNIVERSITY_ENDPOINT))
+				.andExpectAll(
+						// Test the status code, content type, and JSON object count
+						status().isOk(),
+						header().string("Content-Type", "application/json"),
+						jsonPath("$.length()").value(universityCount),
+						content().json(objectMapper.writeValueAsString(universityList)))
 				.andReturn()
 				.getResponse();
-
-		// Test the status code, content type, and JSON content of the response
-		assertThat(response.getStatus()).isEqualTo(200);
-		assertThat(response.getContentType()).isEqualTo("application/json");
-
-		String expectedJSON = objectMapper.writeValueAsString(universityList);
-		String actualJSON = response.getContentAsString();
-		assertThat(actualJSON).isEqualTo(expectedJSON);
 	}
 
 	@Test
@@ -80,49 +78,35 @@ public class UniversityControllerTest {
 		String moduleUriTemplate = String.format("%s/1%s/{moduleId}",
 				UNIVERSITY_ENDPOINT, MODULE_ENDPOINT);
 
-		LocalDate todaysDate = LocalDate.now();
-		University university = new University(
-				1L,
-				"University 1",
-				"Country 1",
-				"Department 1",
-				"Contact Person 1",
-				11,
-				21,
-				todaysDate,
-				todaysDate,
-				URI.create("http://localhost:8080/universities/1/modules"),
-				URI.create("http://localhost:8080/universities/1"),
-				Set.of());
+		University university = generateUniversityObject(1);
 		when(universityService.getUniversityById(1L)).thenReturn(university);
 
-		var response = mockMvc.perform(get(UNIVERSITY_ENDPOINT + "/1"))
-				.andReturn()
-				.getResponse();
-
-		String putUpdateUniversityLinkHeader = HypermediaGenerator.formatLinkHeader(formattedEndpoint,
+		String putUpdateUniversityLinkHeader = formatLinkHeader(formattedEndpoint,
 				"putUpdateUniversity");
-		String delUniversityLinkHeader = HypermediaGenerator.formatLinkHeader(formattedEndpoint, "delUniversity");
-		String putLinkModuleLinkHeader = HypermediaGenerator.formatLinkHeader(moduleUriTemplate, "putLinkModule");
-		String delUnlinkModuleLinkHeader = HypermediaGenerator.formatLinkHeader(moduleUriTemplate, "delUnlinkModule");
-		String getModuleOfUniversityLinkHeader = HypermediaGenerator.formatLinkHeader(moduleUriTemplate,
+		String delUniversityLinkHeader = formatLinkHeader(formattedEndpoint, "delUniversity");
+		String putLinkModuleLinkHeader = formatLinkHeader(moduleUriTemplate, "putLinkModule");
+		String delUnlinkModuleLinkHeader = formatLinkHeader(moduleUriTemplate, "delUnlinkModule");
+		String getModuleOfUniversityLinkHeader = formatLinkHeader(moduleUriTemplate,
 				"getModuleOfUniversity");
 
-		// Test headers and status code
-		assertThat(response.getStatus()).isEqualTo(200);
-		assertThat(response.getContentType()).isEqualTo("application/json");
-		assertThat(response.getHeaders("Link")).contains(
-				putUpdateUniversityLinkHeader,
-				delUniversityLinkHeader,
-				putLinkModuleLinkHeader,
-				delUnlinkModuleLinkHeader,
-				getModuleOfUniversityLinkHeader);
-
-		// Test the JSON content of the response
 		String expectedJSON = objectMapper.writeValueAsString(university);
-		String actualJSON = response.getContentAsString();
 
-		assertThat(actualJSON).isEqualTo(expectedJSON);
+		mockMvc.perform(
+				get(UNIVERSITY_ENDPOINT + "/1"))
+				.andExpectAll(
+						// Test the status code, content type, and Link headers
+						header().stringValues("Link",
+								putUpdateUniversityLinkHeader,
+								delUniversityLinkHeader,
+								putLinkModuleLinkHeader,
+								delUnlinkModuleLinkHeader,
+								getModuleOfUniversityLinkHeader),
+						status().isOk(),
+						header().string("Content-Type", "application/json"),
+						jsonPath("$").exists(),
+						content().json(expectedJSON))
+				.andReturn()
+				.getResponse();
 	}
 
 	@Test
@@ -130,25 +114,25 @@ public class UniversityControllerTest {
 		String universityJson = generateUniversityJsonWithID(1);
 
 		University university = objectMapper.readValue(universityJson, University.class);
-		URI location = URI.create("http://localhost:8080/universities/1");
+		URI location = URI.create(BASE_URI + UNIVERSITY_ENDPOINT + "/1");
 		when(universityService.createUniversity(university)).thenReturn(location);
 
-		var response = mockMvc
-				.perform(post(UNIVERSITY_ENDPOINT)
+		String expectedJSON = objectMapper.writeValueAsString(university);
+
+		mockMvc.perform(
+				post(UNIVERSITY_ENDPOINT)
 						.content(universityJson)
 						.contentType(MediaType.APPLICATION_JSON))
+				.andExpectAll(
+						// Test the status code, Location header, Content-Type, and JSON object
+						status().isCreated(),
+						header().string("Location", location.toString()),
+						header().string("Content-Type", "application/json"),
+						header().doesNotExist("Link"),
+						jsonPath("$").exists(),
+						content().json(expectedJSON))
 				.andReturn()
 				.getResponse();
-
-		// Test headers and status code
-		assertThat(response.getStatus()).isEqualTo(201);
-		assertThat(response.getHeader("Location")).isEqualTo(location.toString());
-
-		// Test the JSON content of the response
-		String expectedJSON = objectMapper.writeValueAsString(university);
-		String actualJSON = response.getContentAsString();
-
-		assertThat(actualJSON).isEqualTo(expectedJSON);
 	}
 
 	@Test
@@ -157,60 +141,58 @@ public class UniversityControllerTest {
 		University universityObject = objectMapper.readValue(universityJson1, University.class);
 
 		// Test creating a new University with a PUT request
+		Optional<URI> locationOptional = Optional.of(URI.create(BASE_URI + UNIVERSITY_ENDPOINT + "/1"));
 		when(universityService.updateUniversity(1L, universityObject))
-				.thenReturn(Optional.of(URI.create("http://localhost:8080/universities/1")));
+				.thenReturn(locationOptional);
 
-		var createResponse = mockMvc.perform(
+		String expectedJSON = objectMapper.writeValueAsString(universityObject);
+		mockMvc.perform(
 				put(UNIVERSITY_ENDPOINT + "/1")
 						.content(universityJson1)
 						.contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$").exists())
+				.andExpectAll(
+						// Test the status code, Location header, Content-Type, and JSON object
+						status().isCreated(),
+						header().string("Location", locationOptional.get().toString()),
+						header().string("Content-Type", "application/json"),
+						header().doesNotExist("Link"),
+						jsonPath("$").exists(),
+						content().json(expectedJSON))
 				.andReturn()
 				.getResponse();
-
-		assertThat(createResponse.getStatus()).isEqualTo(201);
-		assertThat(createResponse.getHeader("Location")).isEqualTo("http://localhost:8080/universities/1");
-
-		String expectedJSON = objectMapper.writeValueAsString(universityObject);
-		String actualJSON = createResponse.getContentAsString();
-
-		assertThat(actualJSON).isEqualTo(expectedJSON);
 
 		// Test updating an existing University with a PUT request
 		when(universityService.updateUniversity(1L, universityObject))
 				.thenReturn(Optional.empty());
 
-		var updateResponse = mockMvc.perform(
+		String getSelfHeaderLink = formatLinkHeader(UNIVERSITY_ENDPOINT + "/1", "getSelf");
+		mockMvc.perform(
 				put(UNIVERSITY_ENDPOINT + "/1")
 						.content(universityJson1)
 						.contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$").doesNotExist())
+				.andExpectAll(
+						// Test the status code, Location header, Content-Type, and JSON object
+						status().isNoContent(),
+						header().doesNotExist("Location"),
+						header().string("Link", getSelfHeaderLink),
+						jsonPath("$").doesNotExist())
 				.andReturn()
 				.getResponse();
-
-		String getSelfHeaderLink = String.format("<%s%s>; rel=\"%s\"", BASE_URI, UNIVERSITY_ENDPOINT + "/1", "getSelf");
-
-		assertThat(updateResponse.getStatus()).isEqualTo(204);
-		assertThat(updateResponse.getContentLength()).isEqualTo(0);
-		assertThat(updateResponse.getHeader("Link")).isEqualTo(getSelfHeaderLink);
-
 	}
 
 	@Test
 	public void testDelete() throws Exception {
-		var response = mockMvc.perform(
-				delete(UNIVERSITY_ENDPOINT + "/1"))
-				.andExpect(jsonPath("$").doesNotExist())
+		String getUniversitiesCollectionHeaderLink = formatLinkHeader(UNIVERSITY_ENDPOINT, "getUniversitiesCollection");
+
+		mockMvc.perform(delete(UNIVERSITY_ENDPOINT + "/1"))
+				.andExpectAll(
+						// Test the status code, Content-Length, and Link header
+						status().isNoContent(),
+						header().string("Link", getUniversitiesCollectionHeaderLink),
+						jsonPath("$").doesNotExist())
 				.andReturn()
 				.getResponse();
-
-		String getUniversitiesCollectionHeaderLink = String.format("<%s%s>; rel=\"%s\"",
-				BASE_URI, UNIVERSITY_ENDPOINT,
-				"getUniversitiesCollection");
-
-		assertThat(response.getStatus()).isEqualTo(204);
-		assertThat(response.getContentLength()).isEqualTo(0);
-		assertThat(response.getHeader("Link")).isEqualTo(getUniversitiesCollectionHeaderLink);
+		;
 	}
 
 	private static String generateUniversityJsonWithID(int id) {
@@ -227,23 +209,27 @@ public class UniversityControllerTest {
 				"}";
 	}
 
+	private static University generateUniversityObject(int id) {
+		return new University(
+				(long) id,
+				"University " + id,
+				"Country " + id,
+				"Department " + id,
+				"Contact Person " + id,
+				10 + id,
+				20 + id,
+				LocalDate.now(),
+				LocalDate.now(),
+				URI.create(BASE_URI + UNIVERSITY_ENDPOINT + "/" + id + MODULE_ENDPOINT),
+				URI.create(BASE_URI + UNIVERSITY_ENDPOINT + "/" + id),
+				Set.of());
+	}
+
 	private static List<University> generateUniversityList(int count) {
 		List<University> list = new ArrayList<>();
 
 		for (int i = 0; i < count; i++) {
-			list.add(new University(
-					(long) i + 1,
-					"University " + i,
-					"Country " + i,
-					"Department " + i,
-					"Contact Person " + i,
-					10 + i,
-					20 + i,
-					LocalDate.now(),
-					LocalDate.now(),
-					URI.create("http://localhost:8080/" + i + "/modules"),
-					URI.create("http://localhost:8080/universities/" + i),
-					Set.of()));
+			list.add(generateUniversityObject(i));
 		}
 
 		return list;
