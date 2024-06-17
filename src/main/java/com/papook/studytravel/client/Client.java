0ea4b38678176f40002d6605e64772897093ff09
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-17 19:50:24 +0400

    added some fields to save relevant data

--- src/main/java/com/papook/studytravel/client/Client.java
+++ src/main/java/com/papook/studytravel/client/Client.java
@@ -1,10 +1,26 @@
 package com.papook.studytravel.client;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Client {
     public final String DISPATCHER_URI;
 
     public Client(final String dispatcherUri) {
         this.DISPATCHER_URI = dispatcherUri;
     }
+
+    public String getUniversitiesCollectionUri;
+    public String getStudyModulesCollectionUri;
+
+    public Map<Long, String> universityLinksOnCurrentPage = new HashMap<>();
+    public Map<Long, String> studyModuleLinksOnCurrentPage = new HashMap<>();
+
+    public HttpClient client;
+    public HttpRequest request;
+    public HttpResponse<String> response;
 
 }
