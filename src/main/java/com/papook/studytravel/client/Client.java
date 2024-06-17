9ee55a0222ce530476257e4516105a2c6e1ed081
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-17 19:50:06 +0400

    added dispatcher uri constant and a constructor

--- src/main/java/com/papook/studytravel/client/Client.java
+++ src/main/java/com/papook/studytravel/client/Client.java
@@ -1,5 +1,10 @@
 package com.papook.studytravel.client;
 
 public class Client {
+    public final String DISPATCHER_URI;
+
+    public Client(final String dispatcherUri) {
+        this.DISPATCHER_URI = dispatcherUri;
+    }
 
 }
