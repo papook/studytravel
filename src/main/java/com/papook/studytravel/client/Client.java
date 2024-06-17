3baa078fd7e2f91ee032f4053e89c94473b15978
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-17 19:50:46 +0400

    added getdispatcher method and a logger

--- src/main/java/com/papook/studytravel/client/Client.java
+++ src/main/java/com/papook/studytravel/client/Client.java
@@ -1,11 +1,15 @@
 package com.papook.studytravel.client;
 
+import java.io.IOException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.Map;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class Client {
     public final String DISPATCHER_URI;
 
@@ -23,4 +27,20 @@ public class Client {
     public HttpRequest request;
     public HttpResponse<String> response;
 
+    public void getDispatcher() {
+        request = HttpRequest.newBuilder()
+                .uri(java.net.URI.create(DISPATCHER_URI))
+                .GET()
+                .build();
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            log.error("Error sending request to dispatcher. Make sure the server is running.");
+        } catch (InterruptedException e) {
+            log.error("The request was interrupted.");
+        }
+    }
+
+    
 }
