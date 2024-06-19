408eab3b4cd74d6bb567d6a6320c148fbc96101c
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-20 00:03:04 +0400

    updated script to try to run the container only if the build command exits successfully

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,4 @@
 
 CONTAINER_NAME=studytravel
 
-docker buildx build . -t $CONTAINER_NAME
-docker run -it -p 8080:8080 $CONTAINER_NAME
+docker buildx build . -t $CONTAINER_NAME && docker run -it -p 8080:8080 $CONTAINER_NAME
